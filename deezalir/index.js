93ded82bd28478b48214bed2800bba3b90601680
ksksksiskskskwowoskskmsmsisisiskskskjsq <89317872+ksksksiskskskwowoskskmsmsisisiskskskjsq@users.noreply.github.com>
2022-03-13 16:10:53 +0500

    Create index.js

---
+++ deezalir/index.js
@@ -0,0 +1,9 @@
+let pass = 30739392748392738286343053
+       let cpass = prompt("Password");
+       if (pass == cpass) {
+         alert("Redirect to page...")
+         window.location.href = '/твлвдааовов'
+       } else {
+         alert("неправильно");
+         window.location.reload();
+       }
